26d95d24da2323454c35fc94af0a7c44037587a2
cockerel <misha.sharai@gmail.com>
2014-05-27 21:00:45 +0300

    Added time sleep for experiment 1 and redesigne

--- StudyOfSubthresholdPerception/StudyOfSubthresholdPerception/FormsExperiments/FormExperiment5.Designer.cs
+++ StudyOfSubthresholdPerception/StudyOfSubthresholdPerception/FormsExperiments/FormExperiment5.Designer.cs
@@ -49,6 +49,16 @@
             this.pictureBoxPresentation = new System.Windows.Forms.PictureBox();
             this.pictureBoxMask = new System.Windows.Forms.PictureBox();
             this.buttonNext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.panelMain.SuspendLayout();
             this.tabControl.SuspendLayout();
             this.tabPageEX1.SuspendLayout();
@@ -92,7 +102,7 @@
             this.tabControl.Controls.Add(this.tabPageEX3);
             this.tabControl.Location = new System.Drawing.Point(3, 3);
             this.tabControl.Name = "tabControl";
-            this.tabControl.SelectedIndex = 2;
+            this.tabControl.SelectedIndex = 0;
             this.tabControl.Size = new System.Drawing.Size(754, 496);
             this.tabControl.TabIndex = 0;
             // 
@@ -118,6 +128,11 @@
             // tabPageEX2
             // 
             this.tabPageEX2.BackColor = System.Drawing.SystemColors.Window;
+            this.tabPageEX2.Controls.Add(this.label8);
+            this.tabPageEX2.Controls.Add(this.label7);
+            this.tabPageEX2.Controls.Add(this.label3);
+            this.tabPageEX2.Controls.Add(this.label2);
+            this.tabPageEX2.Controls.Add(this.label1);
             this.tabPageEX2.Controls.Add(this.labelScaleTest);
             this.tabPageEX2.Controls.Add(this.trackBarScaleTest);
             this.tabPageEX2.Controls.Add(this.labelNumSampleTest);
@@ -131,20 +146,20 @@
             // 
             // labelScaleTest
             // 
-            this.labelScaleTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelScaleTest.Location = new System.Drawing.Point(216, 415);
+            this.labelScaleTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScaleTest.Location = new System.Drawing.Point(283, 430);
             this.labelScaleTest.Name = "labelScaleTest";
-            this.labelScaleTest.Size = new System.Drawing.Size(309, 43);
+            this.labelScaleTest.Size = new System.Drawing.Size(309, 35);
             this.labelScaleTest.TabIndex = 4;
             this.labelScaleTest.Text = "Оценка: 0";
             // 
             // trackBarScaleTest
             // 
-            this.trackBarScaleTest.Location = new System.Drawing.Point(3, 367);
+            this.trackBarScaleTest.Location = new System.Drawing.Point(122, 382);
             this.trackBarScaleTest.Maximum = 100;
             this.trackBarScaleTest.Minimum = -100;
             this.trackBarScaleTest.Name = "trackBarScaleTest";
-            this.trackBarScaleTest.Size = new System.Drawing.Size(740, 45);
+            this.trackBarScaleTest.Size = new System.Drawing.Size(500, 45);
             this.trackBarScaleTest.TabIndex = 1;
             this.trackBarScaleTest.Scroll += new System.EventHandler(this.trackBarScaleTest_Scroll);
             // 
@@ -181,6 +196,11 @@
             // tabPageEX3
             // 
             this.tabPageEX3.BackColor = System.Drawing.SystemColors.Window;
+            this.tabPageEX3.Controls.Add(this.label9);
+            this.tabPageEX3.Controls.Add(this.label10);
+            this.tabPageEX3.Controls.Add(this.label4);
+            this.tabPageEX3.Controls.Add(this.label5);
+            this.tabPageEX3.Controls.Add(this.label6);
             this.tabPageEX3.Controls.Add(this.labelScale);
             this.tabPageEX3.Controls.Add(this.trackBarScale);
             this.tabPageEX3.Controls.Add(this.labelNumTest);
@@ -195,20 +215,20 @@
             // 
             // labelScale
             // 
-            this.labelScale.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelScale.Location = new System.Drawing.Point(216, 417);
+            this.labelScale.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScale.Location = new System.Drawing.Point(283, 430);
             this.labelScale.Name = "labelScale";
-            this.labelScale.Size = new System.Drawing.Size(309, 43);
+            this.labelScale.Size = new System.Drawing.Size(309, 35);
             this.labelScale.TabIndex = 8;
             this.labelScale.Text = "Оценка: 0";
             // 
             // trackBarScale
             // 
-            this.trackBarScale.Location = new System.Drawing.Point(3, 369);
+            this.trackBarScale.Location = new System.Drawing.Point(124, 382);
             this.trackBarScale.Maximum = 100;
             this.trackBarScale.Minimum = -100;
             this.trackBarScale.Name = "trackBarScale";
-            this.trackBarScale.Size = new System.Drawing.Size(740, 45);
+            this.trackBarScale.Size = new System.Drawing.Size(500, 45);
             this.trackBarScale.TabIndex = 7;
             this.trackBarScale.Scroll += new System.EventHandler(this.trackBarScale_Scroll);
             // 
@@ -274,6 +294,96 @@
             this.buttonNext.UseVisualStyleBackColor = true;
             this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "-100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(597, 413);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "100";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(365, 413);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(368, 413);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(599, 414);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "100";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(121, 413);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "-100";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(207, 366);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Негативная оценка";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(451, 366);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Позитивная оценка";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(443, 369);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(107, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Позитивная оценка";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(199, 369);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(106, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Негативная оценка";
+            // 
             // FormExperiment5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -327,5 +437,15 @@
         private System.Windows.Forms.TrackBar trackBarScaleTest;
         private System.Windows.Forms.Label labelScale;
         private System.Windows.Forms.TrackBar trackBarScale;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
