a0f14c67df5f7910be7d4e8dac6b9f4569ceba53
iamburic <iamburic@gmail.com>
2014-10-13 22:47:11 +0300

    UI changes of FormAddDataExp5

--- StudyOfSubthresholdPerception/StudyOfSubthresholdPerception/FormsAddData/FormAddDataToExperiment5.Designer.cs
+++ StudyOfSubthresholdPerception/StudyOfSubthresholdPerception/FormsAddData/FormAddDataToExperiment5.Designer.cs
@@ -77,7 +77,7 @@
             this.buttonAddNeutral.Name = "buttonAddNeutral";
             this.buttonAddNeutral.Size = new System.Drawing.Size(240, 40);
             this.buttonAddNeutral.TabIndex = 3;
-            this.buttonAddNeutral.Text = "Добавить нейтральное изображение";
+            this.buttonAddNeutral.Text = "Выбрать нейтральное изображение";
             this.buttonAddNeutral.UseVisualStyleBackColor = true;
             this.buttonAddNeutral.Click += new System.EventHandler(this.buttonAddNeutral_Click);
             // 
@@ -87,7 +87,7 @@
             this.buttonAddPositive.Name = "buttonAddPositive";
             this.buttonAddPositive.Size = new System.Drawing.Size(240, 40);
             this.buttonAddPositive.TabIndex = 4;
-            this.buttonAddPositive.Text = "Добавить позитивное изображение";
+            this.buttonAddPositive.Text = "Выбрать позитивное изображение";
             this.buttonAddPositive.UseVisualStyleBackColor = true;
             this.buttonAddPositive.Click += new System.EventHandler(this.buttonAddPositive_Click);
             // 
@@ -97,18 +97,18 @@
             this.buttonAddNegative.Name = "buttonAddNegative";
             this.buttonAddNegative.Size = new System.Drawing.Size(240, 40);
             this.buttonAddNegative.TabIndex = 5;
-            this.buttonAddNegative.Text = "Добавить негативное изображение";
+            this.buttonAddNegative.Text = "Выбрать негативное изображение";
             this.buttonAddNegative.UseVisualStyleBackColor = true;
             this.buttonAddNegative.Click += new System.EventHandler(this.buttonAddNegative_Click);
             // 
             // buttonSave
             // 
-            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonSave.Location = new System.Drawing.Point(259, 330);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(240, 40);
             this.buttonSave.TabIndex = 6;
-            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.Text = "Добавить";
             this.buttonSave.UseVisualStyleBackColor = true;
             this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
