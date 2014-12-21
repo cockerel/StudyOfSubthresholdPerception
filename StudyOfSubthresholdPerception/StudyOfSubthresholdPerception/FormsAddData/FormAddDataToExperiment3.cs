fc432b59ac24361f76b23e8e8a82b7698d55a4d8
kreviuz <benderbroun@gmail.com>
2014-12-21 23:20:25 +0300

    Ex2

--- StudyOfSubthresholdPerception/StudyOfSubthresholdPerception/FormsAddData/FormAddDataToExperiment3.cs
+++ StudyOfSubthresholdPerception/StudyOfSubthresholdPerception/FormsAddData/FormAddDataToExperiment3.cs
@@ -19,15 +19,23 @@ namespace StudyOfSubthresholdPerception
         private void buttonAdd_Click(object sender, EventArgs e)
         {
             var ex3 = new Experiment3DataHelper();
-            string und = textBoxEx3Und.Text;
-            string word1 = textBoxEx3Down1.Text;
-            string word2 = textBoxEx3Down2.Text;
-            ex3.AddData(new Experiment3DataModel { FirstAnswer = word1, SecondAnswer = word2, Text = und });
-            formSetEditor.Ex3Load();
-            textBoxEx3Und.Text = string.Empty;
-            textBoxEx3Und.Focus();
-            textBoxEx3Down1.Text = string.Empty;
-            textBoxEx3Down2.Text = string.Empty;
+            if (string.IsNullOrEmpty(textBoxEx3Und.Text) || string.IsNullOrEmpty(textBoxEx3Down1.Text) ||
+                string.IsNullOrEmpty(textBoxEx3Down2.Text))
+            {
+                MessageBox.Show("Заполните все поля ввода данных");
+            }
+            else
+            {
+                string und = textBoxEx3Und.Text;
+                string word1 = textBoxEx3Down1.Text;
+                string word2 = textBoxEx3Down2.Text;
+                ex3.AddData(new Experiment3DataModel {FirstAnswer = word1, SecondAnswer = word2, Text = und});
+                formSetEditor.Ex3Load();
+                textBoxEx3Und.Text = string.Empty;
+                textBoxEx3Und.Focus();
+                textBoxEx3Down1.Text = string.Empty;
+                textBoxEx3Down2.Text = string.Empty;
+            }
         }
 
         private void label21_Click(object sender, EventArgs e)
